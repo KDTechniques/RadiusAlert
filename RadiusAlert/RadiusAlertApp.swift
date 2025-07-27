48df540867f5692a84a932bfe7509486a2b678f4
Kavinda Dilshan <kdtechniques@gmail.com>
2025-07-27 14:38:36 +0530

    Initial Commit

---
+++ RadiusAlert/RadiusAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiusAlertApp.swift
+//  RadiusAlert
+//
+//  Created by Kavinda Dilshan on 2025-07-27.
+//
+
+import SwiftUI
+
+@main
+struct RadiusAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
