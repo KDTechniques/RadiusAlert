d3ab8f27c2a5be05db9b307515110248d47aaed2
Kavinda Dilshan <kdtechniques@gmail.com>
2025-11-05 22:11:29 +0530

    [Update] some UI changes

--- RadiusAlert/UI/Location Pins/Views/Sheet/LocationPinsListView.swift
+++ RadiusAlert/UI/Location Pins/Views/Sheet/LocationPinsListView.swift
@@ -9,20 +9,45 @@ import SwiftUI
 
 struct LocationPinsListView: View {
     @State var mockArray: [LocationPinsModel] = LocationPinsModel.mock
+    @State private var mode: EditMode = .inactive
+    @State private var canRename: Bool = false
     
     // MARK: - BODY
     var body: some View {
         NavigationStack {
             List {
-                ForEach(mockArray) {
-                    Text($0.title)
+                ForEach(mockArray) { item in
+                    if canRename {
+                        NavigationLink(item.title) {
+                            Text("// Rename the location pin and set new radius here...")
+                        }
+                    } else {
+                        Text(item.title)
+                    }
                 }
                 .onDelete(perform: onDelete)
                 .onMove(perform: onMove)
             }
-            .toolbar { EditButton() }
+            .onDisappear {
+                canRename = false
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(canRename ? "Cancel" : "Rename") {
+                        canRename.toggle()
+                    }
+                }
+            }
+            .environment(\.editMode, $mode)
             .navigationTitle(.init("Pined Locations"))
             .navigationBarTitleDisplayMode(.inline)
+            .onChange(of: mode.isEditing) {
+                // use this closure to update the new order of the location pins or try to use `movePins` function if possible.
+            }
         }
     }
 }
